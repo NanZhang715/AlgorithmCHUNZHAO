d71ceda00b63f12099c849a8376f20fc7de628f0
nzhang <nan.zhang@tendcloud.com>
2021-01-25 16:49:47 +0800

    week2, 堆排和快排，以及快排优化

--- Week_02/getLeastNumbers.py
+++ Week_02/getLeastNumbers.py
@@ -40,51 +40,52 @@ class Solution:
         """
         思路:
         1. 快排序
-            时间复杂度：O(n)
+            时间复杂度：O(n), 第一次遍历 O(n), 第二次 O(n/2),  归纳为 O(n+ n/2 + n/4 + ... n/n) =  2n,
+            最坏情况 O(n^2)
             空间复杂度 O(1)
         """
         if not arr:
             return []
         if k >= len(arr):
-            return arr
+            return arr[:k]
 
-        left, right = 0, len(arr)
-        while left < right:
-            pivot = self.patition(arr, left, right)
+        left, right = 0, len(arr) - 1  # 此处为长度减一
+        while left <= right:
+            pivot = self.partition(arr, left, right)
             if pivot == k:
                 return arr[:k]
-            elif pivot > k:  # 右边界不断逼近
-                right = pivot
-            else:  # pivot 小于 k，左边界不断试探
+            elif pivot > k:  #
+                right = pivot - 1
+            else:  # pivot 小于 k，左边界不断收缩
                 left = pivot + 1
+            print(pivot, left, right - 1, arr)
 
-    def getLeastNumbers_pivot_recursive(self, arr: List[int], k: int) -> List[int]:
-        """
-        思路:
-        1. 快排序 上面的快拍空间复杂度可以继续优化, 使用递归增加空间复杂度, 空间还时间，
-        而是近似于二分法,
-        做如下修改
-            时间复杂度：O(logn)
-            空间复杂度 O(n)
-        """
-        if not arr: return []
-        if k >= len(arr): return arr
+    # def getLeastNumbers_pivot_recursive(self, arr: List[int], k: int) -> List[int]:
+    #     """
+    #     思路:
+    #     1. 快排序 上面的快拍空间复杂度可以继续优化, 使用递归增加空间复杂度, 空间还时间，
+    #     而是近似于二分法,
+    #     做如下修改
+    #         时间复杂度：O(logn)
+    #         空间复杂度 O(n)
+    #     """
+    #     if not arr: return []
+    #     if k >= len(arr): return arr
+    #     self.recursive_search(arr, k, 0, len(arr))
+    #     return arr[:k]
+    #
+    # def recursive_search(self, arr, k, left, right):
+    #
+    #     pivot = self.partition(arr, left, right)  # pivot 为索引位置
+    #     print(pivot, arr)
+    #     if pivot == k:
+    #         return
+    #     elif pivot > k:  # k 在 pivot 左侧， 因此在 lower parts 左半边查找
+    #         return self.recursive_search(arr, k, left, pivot)
+    #     else:  # k 在 pivot 右侧 higher parts 寻找
+    #         return self.recursive_search(arr, k, pivot + 1, right)
 
-        return self.recursive_search(arr, k, 0, len(arr))
-
-    def recursive_search(self, arr, k, left, right):
-
-        pivot = self.patition(arr, left, right)  # pivot 为索引位置
-        print(arr, pivot)
-        print("left -> right", left, right)
-        if pivot == k:
-            return arr[:k]
-        elif pivot > k:  # k 在 pivot 左侧， 因此在 lower parts 左半边查找
-            return self.recursive_search(arr, k, left, pivot - 1)
-        else:  # k 在 pivot 右侧 higher parts 寻找
-            return self.recursive_search(arr, k - pivot, pivot + 1, right)
-
-    def patition(self, nums, begin, end):
+    def partition(self, nums, begin, end):
 
         """
          以 pivot 为界， 较大的值放右侧，较小的值放左侧，最终 返回 pivot 的索引
@@ -92,10 +93,10 @@ class Solution:
 
         pivot_index = begin
         pivot = nums[pivot_index]
-        left, right = pivot_index + 1, end - 1
+        left, right = pivot_index + 1, end
 
         while True:
-            if left <= right and nums[left] <= pivot:  # 相同的值，左指针也向右移动一步
+            if left <= right and nums[left] <= pivot:  # 小于等于 pivot 的值， 放在前半部分
                 left += 1
             if right >= left and nums[right] > pivot:
                 right -= 1
@@ -111,6 +112,7 @@ class Solution:
 
 
 if __name__ == '__main__':
-    nums, k = [0, 1, 2, 1], 1
-    rst = Solution().getLeastNumbers_pivot_recursive(nums, k)
-    print("result is", rst)
+    nums, k = [0, 0, 1, 2, 4, 2, 2, 3, 1, 4], 8
+    rst = Solution().getLeastNumbers_pivot(nums, k)
+    # rst = Solution().partition(nums, 0, len(nums))
+    print("result is", rst, nums)
