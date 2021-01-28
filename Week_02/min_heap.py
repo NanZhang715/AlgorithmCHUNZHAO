f3b0b6a226a267321d6c98b44eb3f125c53937d2
nzhang <nan.zhang@tendcloud.com>
2021-01-28 15:05:00 +0800

    week2, 堆的实现

--- Week_02/min_heap.py
+++ Week_02/min_heap.py
@@ -7,7 +7,7 @@
 
 性质：
     （1）根节点 root， 数组索引从 0 开始， root_node | 0
-    （2）父节点 (i-1)/2, 返回父节点 , parent_node | (i-1)/2
+    （2）父节点 (i-1)/2, 返回父节点 , parent_node | i/2 -1
     （3）左节点：left_inidex = (2 * i) + 1,  left_node | 2i + 1
     （4）右节点：right_inidex = (2 * i) + 2,  right_node | 2i +2
 
@@ -33,12 +33,15 @@
 时间复杂度：
     （1）heapify: O(logn): 每次交换操作都是和子节点进行对比，复杂度为 tree 的高度
     （2）build_heapify: O(n)
-        设置树的高度为 h，
-                第一层的节点数量为 2^0
-                第一层的节点数量为 2^1
+        heap 中节点数量是 n，树高为 h(logn), 根节点下面的两个节点，需要向下调整 logn -1, 第三层的4个节点需要调整
+        logn -2
+                第一层的节点数量为 2^0 * logn
+                第一层的节点数量为 2^1 * logn -1
                 第n层的节点数量为 2^h
         构造 heap 中需要对每个元素进行 heapify
-        O(2^0 + 2^1 + ... + 2^h)  = O(n)
+        sum = 1 * logn + 2*logn -1 + 4* logn -2... +
+        2sum = 2 * logn + 4*logn -1 +8*logn -2... +
+        2sum -sum = 2 + 4 + 8 + 2^logn = 2n
 """
 
 
@@ -48,19 +51,18 @@ class MinHeap:
         """
         初始化数组，以及 heap_size
         """
-        self.heap_list = []
+        self.heap_list = [0]
         self.heap_size = 0
 
     def shift_up(self, i):
         """
         与 parent node 进行对比， 向上交换元素， 保持 heap 的性质
         """
-
         # While the element is not the root
         while i // 2 > 0:
-            if self.heap_list[i] < self.heap_list[i - 1 // 2]:
-                self.heap_list[i], self.heap_list[i - 1 // 2] = self.heap_list[i - 1 // 2], self.heap_list[i]
-            i = i - 1 // 2
+            if self.heap_list[i] < self.heap_list[i // 2]:
+                self.heap_list[i], self.heap_list[i // 2] = self.heap_list[(i - 1) // 2], self.heap_list[i]
+            i = i // 2
 
     def push(self, n):
         """
@@ -70,40 +72,53 @@ class MinHeap:
         self.heap_size += 1
         self.shift_up(self.heap_size)
 
-    def shift_down(self,  i):
+    def shift_down(self, i):
 
         """
         与子节点进行对比交换，，保持堆的性质,
         """
         # 如果该节点至少有一个子节点
-        while 2*i + 1 <= self.heap_size:
+        while 2 * i + 1 <= self.heap_size:
             mc = self.min_child(i)
-            if mc.heap_list[i] > self.heap_size[mc]:
+            if self.heap_list[i] > self.heap_list[mc]:
                 self.heap_list[i], self.heap_list[mc] = self.heap_list[mc], self.heap_list[i]
             i = mc
 
     def min_child(self, i):
 
-        # 如果只有子节点
-        if (2*i) + 2 > self.size:
-            return (i * 2) + 1
+        # 如果只有左子节点
+        if (2 * i) + 1 > self.heap_size:
+            return i * 2
         else:
-            if self.heap_list[(i * 2) + 1] < self.heap_list[(i * 2) + 2]:
-                return (i*2) + 1
-            return (i*2) + 2
+            if self.heap_list[i * 2] < self.heap_list[(i * 2) + 1]:
+                return i * 2
+            return (i * 2) + 1
 
     def pop(self):
 
-        if not self.heap_list:
+        if not self.heap_size:
             return "Empty heap"
 
-        root = self.heap_list[0]
-        self.heap_list[0], self.heap_list[-1] = self.heap_list[-1], self.heap_list[0]
+        root = self.heap_list[1]
+        self.heap_list[1], self.heap_list[-1] = self.heap_list[-1], self.heap_list[1]
         self.heap_list.pop()
         self.heap_size -= 1
-        self.shift_down(0)
+        self.shift_down(1)
 
         return root
 
 
+if __name__ == '__main__':
 
+    my_heap = MinHeap()
+    my_heap.push(5)
+    my_heap.push(6)
+    my_heap.push(7)
+    my_heap.push(9)
+    my_heap.push(13)
+    my_heap.push(11)
+    my_heap.push(10)
+    print(my_heap.heap_list)
+    print(my_heap.pop())  # removing min node i.e 5
+    print(my_heap.pop())
+    print(my_heap.heap_list)
